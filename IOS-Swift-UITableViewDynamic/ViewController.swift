709826e52d5146b4bac81dbeebdc6e1a1286a40a
Pooya Hatami <pooya@node-1w7jr9qro06sw04hcipbybhw2.ipv6.telus.net>
2018-09-06 12:27:54 -0700

    done

--- IOS-Swift-UITableViewDynamic/ViewController.swift
+++ IOS-Swift-UITableViewDynamic/ViewController.swift
@@ -8,16 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource {
 
+    var tableData : [String] = []
+    @IBOutlet weak var tableView : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for myIndex in 0 ... 20 {
+            tableData.append("row number \(myIndex)")
+        }
+        
+        tableView.dataSource = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cellText = tableData[indexPath.row]
+        cell.textLabel?.text = cellText
+        
+        if  indexPath.row % 2 == 0 {
+            cell.contentView.backgroundColor = UIColor.lightGray
+        }
+        return cell
     }
 
 
